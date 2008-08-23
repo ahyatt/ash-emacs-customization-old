134d766fdff2ce1a11ac03354c63899a5e4d78b0
Andrew Hyatt <ahyatt@google.com>
2008-08-23 09:59:00 -0400

    Modified erc ignore list - it was ignoring too much

--- .emacs.d/ash/ash-erc.el
+++ .emacs.d/ash/ash-erc.el
@@ -1,20 +1,17 @@
 (require 'erc)
-(require 'erc-page-me)
 
 (setq erc-modules '(autoaway autojoin completion fill irccontrols log match menu move-to-prompt noncommands notify readonly ring scrolltobottom smiley stamp track)
-      erc-hide-list (quote ("JOIN" "KICK" "NICK" "PART" "QUIT" "MODE"
-                            "301" "305" "306" "317"
-                            "324" "329" "332" "333" "353" "477"))
+      erc-hide-list (quote ("JOIN" "KICK" "NICK" "PART" "QUIT" "MODE"))
       erc-autoaway-mode t
       erc-notify-mode t
-      erc-track-when-inactive nil
       erc-echo-notices-in-minibuffer-flag t
       erc-auto-query 'window-noselect
       erc-autoaway-idletimer 'emacs
       erc-user-full-name user-full-name
+      erc-track-when-inactive 'nil
+      erc-track-exclude-types '(("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                 "324" "329" "332" "333" "353" "477"))
       erc-track-exclude-server-buffer t
-      erc-track-showcount t
-      erc-notify-signoff-hook '(erc-notify-signoff)
-      erc-notify-signon-hook '(erc-notify-signon))
+      erc-track-showcount t)
 
 (provide 'ash-erc)
