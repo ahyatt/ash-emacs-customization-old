971da4d8de48d7936ffb2f9dae838ed60361dbe9
Andrew Hyatt <ahyatt@google.com>
2009-12-21 10:53:30 -0500

    Added undo-tree
    Conflicts:
    
    	.emacs.d/ash/ash-customizations.el
    Added zenburn.el
    
    Add jabber elc files

--- .emacs.d/ash/ash-customizations.el
+++ .emacs.d/ash/ash-customizations.el
@@ -193,4 +193,32 @@ and `ash-project-root' variables."
     (error "not ash-project-name or ash-project-root variable set")))
 
 (global-set-key [(f12)] 'ash-ibuffer-project-files)
+
+;; from http://www.emacswiki.org/emacs/RecentFiles
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let* ((file-assoc-list
+	  (mapcar (lambda (x)
+		    (cons (file-name-nondirectory x)
+			  x))
+		  recentf-list))
+	 (filename-list
+	  (remove-duplicates (mapcar #'car file-assoc-list)
+			     :test #'string=))
+	 (filename (ido-completing-read "Choose recent file: "
+					filename-list
+					nil
+					t)))
+    (when filename
+      (find-file (cdr (assoc filename
+			     file-assoc-list))))))
+
+(key-chord-define-global "xb" 'recentf-ido-find-file)
+(key-chord-define-global "xg" 'execute-extended-command)
+
+(require 'undo-tree)
+
+(global-undo-tree-mode)
+
 (provide 'ash-customizations)
